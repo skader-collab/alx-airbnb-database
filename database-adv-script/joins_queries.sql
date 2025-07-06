d5ef87c15da05275a6f7baf52ea9eb08d95f285a
skader <sekouskaderkamara@gmail.com>
2025-07-06 09:14:54 +0000

    left join [ORDERBY] error fix

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,15 +1,22 @@
 -- 1. INNER JOIN: Retrieve all bookings and the respective users who made those bookings
 SELECT Reservations.*, Users.name, Users.email
 FROM Reservations
-INNER JOIN Users ON Reservations.user_id = Users.id;
+INNER JOIN Users ON Reservations.user_id = Users.id
+ORDER BY Reservations.id;
 
 -- 2. LEFT JOIN: Retrieve all properties and their reviews, including properties that have no reviews
 SELECT Rooms.*, Reviews.rating, Reviews.comment, Reviews.created_at
 FROM Rooms
 LEFT JOIN Reservations ON Rooms.id = Reservations.room_id
-LEFT JOIN Reviews ON Reservations.id = Reviews.reservation_id;
+LEFT JOIN Reviews ON Reservations.id = Reviews.reservation_id
+ORDER BY Rooms.id, Reviews.created_at;
 
--- 3. FULL OUTER JOIN: Retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user
+-- 3. FULL OUTER JOIN (emulated with LEFT JOIN and UNION): Retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user
 SELECT Users.*, Reservations.*
 FROM Users
-FULL OUTER JOIN Reservations ON Users.id = Reservations.user_id;
+LEFT JOIN Reservations ON Users.id = Reservations.user_id
+UNION
+SELECT Users.*, Reservations.*
+FROM Reservations
+LEFT JOIN Users ON Reservations.user_id = Users.id
+ORDER BY Users.id, Reservations.id;

1a9457029d73c8bef198c9772e6225cf4f9267b8
skader <sekouskaderkamara@gmail.com>
2025-07-06 09:12:10 +0000

    Write Complex Queries with Joins

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,15 @@
+-- 1. INNER JOIN: Retrieve all bookings and the respective users who made those bookings
+SELECT Reservations.*, Users.name, Users.email
+FROM Reservations
+INNER JOIN Users ON Reservations.user_id = Users.id;
+
+-- 2. LEFT JOIN: Retrieve all properties and their reviews, including properties that have no reviews
+SELECT Rooms.*, Reviews.rating, Reviews.comment, Reviews.created_at
+FROM Rooms
+LEFT JOIN Reservations ON Rooms.id = Reservations.room_id
+LEFT JOIN Reviews ON Reservations.id = Reviews.reservation_id;
+
+-- 3. FULL OUTER JOIN: Retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user
+SELECT Users.*, Reservations.*
+FROM Users
+FULL OUTER JOIN Reservations ON Users.id = Reservations.user_id;
